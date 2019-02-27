80080ad303af2e1c3700d32cf570315befcaf6aa
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2019-02-27 15:50:15 -0600

    added revert option

--- extension_changer.py
+++ extension_changer.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import time
+from six.moves import input as inp
 
 """
 description:
@@ -20,15 +21,18 @@ optional arguments:
   -p PATH     path to dir
   -o OLD      old extension
   -n NEW      new extension
+  -u          undo, revert changes
   -r          is recursive
 
 python extension_changer.py -p /home/syseng/bin/data/examples/ -o exp -n txt
 """
 
 # you can hard code values here if you dont want a bunch of args
-user_path = '/path/here'
-user_old = 'okay'
-user_new = 'ok'
+user_path = './'
+user_old = 'fake'
+user_new = 'fake2'
+
+changelog = './ext_change.log'
 
 
 def get_args():
@@ -42,6 +46,8 @@ def get_args():
     parser.add_argument('-p', dest='path', required=False, help="path to dir")
     parser.add_argument('-o', dest='old', required=False, help="old extension")
     parser.add_argument('-n', dest='new', required=False, help="new extension")
+    parser.add_argument('-u', dest='undo', required=False, action="store_true",
+                        help="revert changes")
     parser.add_argument('-r', dest='recursive', required=False,
                         action="store_true", help="is recursive")
     args = parser.parse_args()
@@ -57,11 +63,15 @@ def get_args():
         new = args.new
     else:
         new = user_new
+    if args.undo:
+        undo = True
+    else:
+        undo = False
     if args.recursive:
         recursive = True
     else:
         recursive = False
-    return path, recursive, old, new
+    return path, recursive, old, new, undo
 
 
 def get_files(recursive, path):
@@ -82,19 +92,59 @@ def get_files(recursive, path):
     return file_list
 
 
+def handle_log():
+    """
+    handles removal / backup of the change log
+    """
+    # if the changelog is there and isn't empty
+    if os.path.exists(changelog) and os.path.getsize(changelog) > 0:
+        print("detected a changelog! if deleted you wont be able to revert!")
+        # here we get the user input 'y/n/b'. some genius decided to
+        # change the input method for python2/3 so you have to use a
+        # compatibility module called six. look how i imported it up
+        # top to see how this was achieved
+        choice = inp("remove? y/n/b (yes/no/backup-and-remove)").lower()
+        if choice == 'y':
+            # try to remove the change log
+            try:
+                os.remove(changelog)
+                # exception is likely file does not exist
+            except Exception as _e:
+                # whatever it is we dont care
+                pass
+        # else they want to keep the file as is
+        elif choice == 'n':
+            print("exiting...")
+            exit()
+        # else they want to back it up and remove
+        elif choice == 'b':
+            # get the unix time for the backup name
+            mod = str(round(time.time()))
+            # append the unix time to the changelog
+            new_changelog = './ext_change' + mod + '.log'
+            # rename it (should remove)
+            os.rename(changelog, new_changelog)
+            # inform the user of the backup log name
+            print('created ' + new_changelog)
+            # try to remove again for whatever reason
+            try:
+                os.remove(changelog)
+                # exception is like file does not exist
+            except Exception as _e:
+                # whatever it is we dont care
+                pass
+            # otherwise they entered in an unsupported option
+            else:
+                print("that wasn't an option, exiting")
+                exit()
+
+
 def change_extension(path, file_list, old, new):
     """
     changes the file extension and logs the modification
     """
-    # try to remove the change log
-    try:
-        os.remove('ext_change.log')
-    # exception is like file does not exist
-    except Exception as _e:
-        # whatever it is we dont care
-        pass
-    # open the change log
-    with open('ext_change.log', 'a') as f:
+    handle_log()
+    with open(changelog, 'a') as f:
         for i in file_list:
             # if the file ends with the old extension
             if i.endswith(old):
@@ -108,13 +158,34 @@ def change_extension(path, file_list, old, new):
                 os.rename(i, newfile)
 
 
+def revert():
+    """
+    using the changelog, reverts files to previous name
+    """
+    # if log exists and is not empty
+    if os.path.exists(changelog) and os.path.getsize(changelog) > 0:
+        with open(changelog) as f:
+            manifest = f.readlines()
+        for i in manifest:
+            old = i.split()[3]
+            new = i.split()[1]
+            print('moving ' + old + ' to ' + new)
+            os.rename(old, new)
+    else:
+        print("couldnt find the change log!!!")
+
+
 def main():
     """
     times the duration of the runtime, gets user args, gets the file
     list, changes the extension, informs user upon completion
     """
     start = time.time()
-    path, recursive, old, new = get_args()
+    path, recursive, old, new, undo = get_args()
+    # if it is an undo, exit afterwards
+    if undo:
+        revert()
+        exit()
     file_list = get_files(recursive, path)
     change_extension(path, file_list, old, new)
     runtime = time.time() - start
